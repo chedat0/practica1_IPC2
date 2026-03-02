221e3594d3a56a056be08549e33df1794ab68e9e
chedat0 <jeffersongomez201830892@cunoc.edu.gt>
2026-03-02 01:22:06 -0600

    Se agrega mapeo de base de datos, asimismo la conexion de java con base de datos y se establece la arquitectura para trabajar, la cual es MVC, (modelo, vista, controlador)

---
+++ prac1_IPC2/src/main/java/com/mycompany/prac1_ipc2/Prac1_IPC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prac1_ipc2;
+
+/**
+ *
+ * @author jeffm
+ */
+public class Prac1_IPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
